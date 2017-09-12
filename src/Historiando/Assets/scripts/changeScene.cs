c80dbfbbe52de855add0f2c9024094177c00172d
inacioMattos <inaciotm@hotmail.com>
2017-09-12 02:19:42 -0300

    [ADD] Big commit
    The basis of the app is done, much left though.

---
+++ src/Historiando/Assets/scripts/changeScene.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour
+{
+	int vidas = 3;
+	public GameObject infoCerto;
+	public GameObject infoErrado;
+
+
+	public void goTo(string scene)
+	{
+		if (scene == "r")
+			correto ();
+
+		else if (scene == "w")
+			errado ();
+
+		else
+			Application.LoadLevel (scene);
+	}
+
+
+	private void proxQuestao()
+	{
+			// something();
+	}
+
+
+	void correto()
+	{
+		float lTime = Time.time;
+		infoCerto.SetActive (true);
+
+
+	}
+
+
+	void errado()
+	{
+		float lTime = Time.time;
+		infoErrado.SetActive (true);
+
+
+	}
+
+}
